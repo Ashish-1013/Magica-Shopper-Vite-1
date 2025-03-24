a3e6d892c0ef75822c052b2cb05cbaa5964b3c99
A K Giri <ashishkumargiri13@gmail.com>
2025-03-24 08:57:38 +0530

    server

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins:[react()],
-    server:{
-        port:3000,   //if we want then use 3000
-        open: true
-    },
+    // server:{
+    //     port:3000,   //if we want then use 3000
+    //     open: true
+    // },
     base: '/Magica-Shopper-Vite-1/',
 });
